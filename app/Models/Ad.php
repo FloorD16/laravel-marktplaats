6253a635c493cc21713249e686f0faaab5d0a61f
FloorD16 <floor.dogger@gmail.com>
2025-09-19 12:48:58 +0200

    Initial commit

---
+++ app/Models/Ad.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ad extends Model
+{
+    public function bids() {
+        return $this->hasMany(Bid::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+}
---
+++ app/Models/Bid.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bid extends Model
+{
+    public function ad() {
+        return belongsTo(Ad::class);
+    }
+
+    public function user() {
+        return belongsTo(User::class);
+    }
+}
